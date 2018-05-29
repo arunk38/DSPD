c003d96a2c77b7eb1eaa6e2319ba9e6a79019c38
Arun Kumar <karun.2210@gmail.com>
2018-05-29 22:03:37 +0530

    Initial code for skip list insert operation

--- DataStructures/LinkedLists/SkipLists/insertion/list_insert.c
+++ DataStructures/LinkedLists/SkipLists/insertion/list_insert.c
@@ -23,7 +23,7 @@ typedef struct skiplist {
  */
 
 void
-sl_print(
+ak_sl_print(
     skiplist        list)
 {
     int i, level = list.level;
@@ -49,7 +49,7 @@ sl_print(
  */
 
 void
-sl_init(
+ak_sl_init(
     skiplist       *list)
 {
     int i;
@@ -92,19 +92,77 @@ randomLevel()
 
 }
 
+/*
+ * Insert into skip list at appropriate level(s)
+ */
+
+void
+ak_sl_insert(
+    skiplist        *list,
+    int             data)
+{
+    sl_node *update[SKIPLIST_MAX_LEVEL + 1];
+    sl_node *x = list->header;
+
+    int i, level;
+
+    /*
+     * Traverse linked list(forward) at each level and store
+     * the previous node pointer at which new data has to be
+     * inserted
+     */
+
+    for (i = list->level; i >= 1; i--) {
+
+        /*
+         * Traverse this level upto node with data greater than
+         * the new `data`
+         */
+
+        while (x->forward[i] && (x->forward[i]->data < data))
+            x = x->forward[i];
+
+        update[i] = x;
+    }
+
+    /*
+     * get level at which new data is to be inserted
+     */
+
+    level = randomLevel();
+
+    if (level > list->level) {
+        for (i = list->level + 1; i <= level; i++) {
+            update[i] = NULL;
+        }
+        list->level = level;
+    }
+
+    x = (sl_node *)malloc(sizeof (sl_node));
+    x->data = data;
+    x->forward = (sl_node **)malloc(sizeof (sl_node*) * (level + 1));
+
+    for (i = 1; i <= level; i++) {
+        x->forward[i] = update[i]->forward[i];
+        update[i]->forward[i] = x;
+    }
+
+}
+
 int
 main()
 {
     skiplist list;
     int data, cnt = 1;
 
-    sl_init(&list);
+    ak_sl_init(&list);
 
     while (cnt) {
         printf("Enter data to insert: ");
-        scanf("%d", &cnt);
+        scanf("%d", &data);
+        ak_sl_insert(&list, data);
         printf("Continue adding? (1/0) ");
         scanf("%d", &cnt);
     }
-    sl_print(list);
+    ak_sl_print(list);
 }
