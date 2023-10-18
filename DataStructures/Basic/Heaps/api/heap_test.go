bfdc99134ac4571302ed3631bf7b5d92cc9d5379
Arun Kumar Kalagarla <karun.2210@gmail.com>
2023-10-18 13:03:05 +0530

    Introduction to heaps
        * Heap data struct with custom compare function
        * Push, Pop, Peek functionalites for Heap data type

---
+++ DataStructures/Basic/Heaps/api/heap_test.go
@@ -0,0 +1,150 @@
+package heap_test
+
+import (
+	"github.com/stretchr/testify/assert"
+	heap "heaps.example.com/api/v1"
+
+	"testing"
+)
+
+func TestHeapify(t *testing.T) {
+	type Person struct {
+		id   int
+		name string
+		age  int
+	}
+	tests := []struct { // test case definition
+		name string
+		args bool
+		got  any
+		want any
+	}{ // test cases
+		{ // test case
+			name: "Max Heap",
+			args: false,
+			got: heap.NewHeap(func(a, b int) bool { return a < b }).
+				BuildHeap([]int{6, 5, 4, 8, 9, 10, 13, 12, 11, 7}).GetHeapData(),
+			want: []int{13, 12, 10, 11, 7, 4, 9, 5, 8, 6},
+		},
+		{ // test case
+			name: "Get size of heap {6, 5, 4, 8, 9, 10, 13, 12, 11, 7}",
+			args: false,
+			got: heap.NewHeap(func(a, b int) bool { return a < b }).
+				BuildHeap([]int{6, 5, 4, 8, 9, 10, 13, 12, 11, 7}).GetHeapSize(),
+			want: 10,
+		},
+		{ // test case
+			name: "Peek Max Heap",
+			args: false,
+			got: heap.NewHeap(func(a, b int) bool { return a < b }).
+				BuildHeap([]int{6, 5, 4, 8, 9, 10, 13, 12, 11, 7}).Peek(),
+			want: 13,
+		},
+		{ // test case
+			name: "Sort in max Heap",
+			args: false,
+			got: heap.NewHeap(func(a, b int) bool { return a < b }).
+				BuildHeap([]int{6, 5, 4, 8, 9, 10, 13, 12, 11, 7, 1}).HeapSort(),
+			want: []int{13, 12, 11, 10, 9, 8, 7, 6, 5, 4, 1},
+		},
+		{ // test case
+			name: "Validate heap push",
+			args: false,
+			got: heap.NewHeap(func(a, b int) bool { return a < b }).
+				BuildHeap([]int{6, 5, 4, 8, 9, 10, 13, 12, 11, 7, 1}).Push(20).GetHeapData(),
+			want: []int{20, 12, 13, 11, 7, 10, 9, 5, 8, 6, 1, 4},
+		},
+		{ // test case
+			name: "Validate heap pop",
+			args: true,
+			got: heap.NewHeap(func(a, b int) bool { return a < b }).
+				BuildHeap([]int{6, 5, 4, 8, 9, 10, 13, 12, 11, 7, 1}).Pop().GetHeapData(),
+			want: []int{12, 11, 10, 5, 7, 4, 9, 1, 8, 6},
+		},
+		{ // test case
+			name: "Min Heap",
+			args: false,
+			got: heap.NewHeap(func(a, b int) bool { return a > b }).
+				BuildHeap([]int{6, 5, 4, 8, 9, 10, 13, 12, 11, 7}).GetHeapData(),
+			want: []int{4, 6, 5, 8, 7, 10, 13, 12, 11, 9},
+		},
+		{ // test case
+			name: "Sort in min Heap",
+			args: false,
+			got: heap.NewHeap(func(a, b int) bool { return a > b }).
+				BuildHeap([]int{6, 5, 4, 8, 9, 10, 13, 12, 11, 7, 1}).HeapSort(),
+			want: []int{1, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13},
+		},
+		{ // test case
+			name: "Empty heap, size is zero",
+			args: false,
+			got:  heap.NewHeap(func(a, b int) bool { return a < b }).GetHeapSize(),
+			want: 0,
+		},
+		{ // test case
+			name: "Peek Min Heap",
+			args: false,
+			got: heap.NewHeap(func(a, b int) bool { return a > b }).
+				BuildHeap([]int{6, 5, 4, 8, 9, 10, 13, 12, 11, 7, 1}).Peek(),
+			want: 1,
+		},
+		{ // test case
+			name: "Get size of heap {5, 10}",
+			args: false,
+			got: heap.NewHeap(func(a, b int) bool { return a < b }).
+				BuildHeap([]int{5, 10}).GetHeapSize(),
+			want: 2,
+		},
+		{ // test case
+			name: "Delete from heap {5, 10}",
+			args: false,
+			got: heap.NewHeap(func(a, b int) bool { return a < b }).
+				BuildHeap([]int{5, 10}).
+				DeleteData().GetHeapSize(),
+			want: 1,
+		},
+		{ // test case
+			name: "Test heap with struct object",
+			args: false,
+			got: heap.NewHeap(func(a, b Person) bool { return a.age < b.age }).
+				BuildHeap([]Person{
+					{1, "Jerry", 20},
+					{2, "Tome", 18},
+					{3, "Nick", 19},
+					{4, "Bob", 34},
+					{5, "Tommy", 36},
+				}).GetHeapData(),
+			want: []Person{
+				{5, "Tommy", 36},
+				{4, "Bob", 34},
+				{3, "Nick", 19},
+				{2, "Tome", 18},
+				{1, "Jerry", 20},
+			},
+		},
+		{ // test case
+			name: "Test heap with struct object",
+			args: false,
+			got: heap.NewHeap(func(a, b Person) bool { return a.age < b.age }).
+				BuildHeap([]Person{
+					{1, "Jerry", 20},
+					{2, "Tome", 18},
+					{3, "Nick", 19},
+					{4, "Bob", 34},
+					{5, "Tommy", 36},
+				}).Pop().GetHeapData(),
+			want: []Person{
+				{4, "Bob", 34},
+				{1, "Jerry", 20},
+				{3, "Nick", 19},
+				{2, "Tome", 18},
+			},
+		},
+	}
+
+	for _, tt := range tests { // test cases executions
+		t.Run(tt.name, func(t *testing.T) { // function call
+			assert.Equal(t, tt.want, tt.got) // test case assertions
+		})
+	}
+}
---
+++ DataStructures/Basic/Heaps/api/v1/api.go
@@ -0,0 +1,158 @@
+package api
+
+import "fmt"
+
+/*
+ * Functionalities of heap
+ */
+
+type (
+	Heap[T any] struct {
+		data     []T
+		comp     func(a, b T) bool
+		heapSize int
+	}
+)
+
+// Initialize a new heap with its comaparision function
+func NewHeap[T any](comp func(a, b T) bool) *Heap[T] {
+	return &Heap[T]{
+		data:     make([]T, 0),
+		comp:     comp,
+		heapSize: 0,
+	}
+}
+
+// Pop return the top value of the heap and rearrange the heap
+// If the heap is empty, return zero value and false
+func (h *Heap[T]) Pop() *Heap[T] {
+	if h.GetHeapSize() == 0 {
+		return nil
+	}
+	h.swap(0, h.GetHeapSize()-1)
+	h.data = h.data[:h.GetHeapSize()-1]
+	h.heapifyDown(0)
+	h.heapSize--
+
+	return h
+}
+
+// heapifyDown moves the value at index i down to its correct position in the heap
+// Assume the left and right branch are already in the correct position in the heap except for i
+// assumes that the binary trees rooted at LEFT.i/ and RIGHT.i/ are max- heaps, but that data at i
+// might fail comparition thus violating the heap property
+func (h *Heap[T]) heapifyDown(idx int) {
+
+	leftChild := childIdx(idx)
+	rightChild := childIdx(idx)
+
+	largest := idx
+
+	// See if left child of root exists and passes comparition
+	if leftChild < len(h.data) && h.comp(h.data[idx], h.data[leftChild]) {
+		largest = leftChild
+	}
+
+	// See if right child of root exists and passes comparition
+	if rightChild < len(h.data) && h.comp(h.data[largest], h.data[rightChild]) {
+		largest = rightChild
+	}
+
+	// swap if current index is modified
+	if largest != idx {
+		h.swap(idx, largest)
+		h.heapifyDown(largest)
+	}
+}
+
+// heapifyUp moves the value at index i up to its correct position in the heap
+// Assume other values are already in the correct position in the heap except for i
+func (h *Heap[T]) heapifyUp(i int) *Heap[T] {
+
+	for h.comp(h.data[parentIdx(i)], h.data[i]) {
+		h.swap(i, parentIdx(i))
+		i = parentIdx(i)
+	}
+
+	return h
+}
+
+// Push an value into the already sorted heap
+func (h *Heap[T]) Push(v T) *Heap[T] {
+	h.data = append(h.data, v)
+	h.heapSize++
+	return h.heapifyUp((len(h.data) - 1))
+}
+
+// heapify an array, and return new heap obj
+func (h *Heap[T]) BuildHeap(input []T) *Heap[T] {
+
+	newHeap := NewHeap(h.comp)
+	for _, v := range input {
+		newHeap = newHeap.Push(v)
+	}
+	return newHeap
+}
+
+// deletes data to heap
+func (h *Heap[T]) DeleteData() *Heap[T] {
+	h.data = h.data[1:]
+	h.heapSize--
+
+	return h
+}
+
+// Prints the heap is array format
+func (h *Heap[T]) PrintHeap() {
+	fmt.Println("Current heap contents: ", h.data)
+}
+
+// sorts the given heap according the comparition function
+// and returned sorted list
+func (h *Heap[T]) HeapSort() []T {
+	var sortedData []T
+
+	tempData := h.data
+	n := h.GetHeapSize()
+
+	for len(sortedData) < n {
+		sortedData = append(sortedData, h.data[0])
+		h = h.BuildHeap(h.data[1:])
+	}
+	h.data = tempData
+
+	return sortedData
+}
+
+// swap swaps the values at indices i and j
+func (h *Heap[T]) swap(i, j int) {
+	h.data[i], h.data[j] = h.data[j], h.data[i]
+}
+
+// swap swaps the values at indices i and j
+func (h *Heap[T]) Peek() T {
+	return h.data[0]
+}
+
+// return the child index of the current heap element
+func childIdx(i int) int {
+	return 2*i + 1
+}
+
+// return the parent element index for current element
+func parentIdx(i int) int {
+	return (i - 1) / 2
+}
+
+func (h *Heap[T]) GetHeapSize() int {
+	return h.heapSize
+}
+
+func (h *Heap[T]) GetHeapData() []T {
+	return h.data
+}
+
+// TODO: Delete
+func Add(a, b int) int {
+	return a + b
+}
