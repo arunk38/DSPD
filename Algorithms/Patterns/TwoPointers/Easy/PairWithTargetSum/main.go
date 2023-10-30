50ff12b52f4ea807867beea15bbc772a762a762c
Arun Kumar Kalagarla <karun.2210@gmail.com>
2023-10-30 14:58:36 +0530

    Algorithem pattern - Two Pointers #1
            * Pair with target sum in sorted array

---
+++ Algorithms/Patterns/TwoPointers/Easy/PairWithTargetSum/main.go
@@ -0,0 +1,36 @@
+package main
+
+import "fmt"
+
+func pairWithTargetSum(intput []int, targetSum int) {
+	output := []int{-1, -1}
+	pairSum := 0
+	startPointer := 0
+	endPointer := len(intput) - 1
+
+	fmt.Println("\nInput array : ", intput, "Target Sum :", targetSum)
+
+	for startPointer <= endPointer {
+		pairSum = intput[startPointer] + intput[endPointer]
+
+		// if current pair sum is greater decrease the end pointer
+		if pairSum > targetSum {
+			endPointer -=1
+		} else if pairSum < targetSum {
+			startPointer += 1 // if current pair sum is less increase the start pointer
+		} else {
+			// target sum in reached, update output
+			output[0] = startPointer
+			output[1] = endPointer
+			// make endpointer negative to break loop
+			endPointer = -1
+		}
+	}
+
+	fmt.Println(output)
+}
+
+func main() {
+	pairWithTargetSum([]int{1, 2, 3, 4, 6}, 6)
+	pairWithTargetSum([]int{2, 5, 9, 11}, 11)	
+}
